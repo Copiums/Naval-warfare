ea3c318d48081f35faaff89050657c55cf0cca22
Copium <119388242+Copiums@users.noreply.github.com>
2026-05-11 15:55:13 -0400

    Update Ship.lua

--- Modules/Ship.lua
+++ Modules/Ship.lua
@@ -1,47 +1,68 @@
 local Ship = {
-    Status = {
-        CurrentGun = -5
-    }
+        Status = {CurrentGun = -5},
+        Ship = nil
 }
 
-local ShipNames = {
-	"Heavy Cruiser",
-	"BattleShip",
-	"Cruiser",
-	"Carrier",
-	"Destroyer",
+local shipNames: table = {
+        "Heavy Cruiser", "Cruiser", "Carrier", "Destroyer", "Battleship", "Submarine"
 }
 
-function Ship:UpdateShip(SeatPart : BasePart)
-    if not SeatPart then
-        self.Ship = nil
-        return
-    end
-    local Parent = SeatPart.Parent
-    if table.find(ShipNames, Parent.Name) then
-        self.Ship = Parent
-    end
-end
-
-function Ship:GetGunBarrel(gunIndex : number)
-    gunIndex = 2 - gunIndex
-    for i,Gun in pairs(self.Ship:GetChildren()) do
-        if Gun.Name ~= "Turret" then
-            continue
+local ShipUpdate: (seatPart: BasePart?) -> () = function(seatPart: BasePart?)
+        if not seatPart then
+                Ship.Ship = nil
+                return
         end
-        if Gun.ID.Value ~= gunIndex then
-            continue
+
+        local model = seatPart.Parent
+        if table.find(shipNames, model.Name) then
+                Ship.Ship = model
         end
-        local PrimeGun = Gun:FindFirstChild("PrimeGun")
-        local BRight = Gun:FindFirstChild("BRight")
-
-        return PrimeGun or BRight
-    end
 end
 
-function Ship:CurrentGun()
-    return Ship.Ship.GunNum.Value
+local GetGunBarrel: (gunIndex: number) -> any = function(gunIndex: number)
+        if not Ship.Ship then return nil end
+
+        -- Main Artillery (Gun 0)
+        if gunIndex == 0 then
+                -- Direct PrimeGun
+                local primeGun: any = Ship.Ship:FindFirstChild("PrimeGun", true)
+                if primeGun then return primeGun end
+
+                -- Inside Turrets
+                for _, turret in next, Ship.Ship:GetDescendants() do
+                        if turret.Name == "Turret" and turret:IsA("Model") then
+                                local pg = turret:FindFirstChild("PrimeGun")
+                                if pg then return pg end
+                                local bright = turret:FindFirstChild("BRight")
+                                if bright then return bright end
+                        end
+                end
+
+                -- Inside Body
+                local body = Ship.Ship:FindFirstChild("Body")
+                if body then
+                        local pg = body:FindFirstChild("PrimeGun", true)
+                        if pg then return pg end
+                end
+        end
+
+        -- Anti-Air (Gun 1)
+        if gunIndex == 1 then
+                return Ship.Ship:FindFirstChild("PrimeGun", true) or Ship.Ship:FindFirstChild("Gun", true)
+        end
+
+        return nil
 end
 
+local GetCurrentGun: () -> number = function()
+        if Ship.Ship and Ship.Ship:FindFirstChild("GunNum") then
+                return Ship.Ship.GunNum.Value
+        end
+        return -1
+end
+
+Ship.UpdateShip = ShipUpdate
+Ship.GetGunBarrel = GetGunBarrel
+Ship.GetCurrentGun = GetCurrentGun
 
 return Ship
