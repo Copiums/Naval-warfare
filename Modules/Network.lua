1bba1c9ddf9177b6d4010537320db40671cc4813
Copium <119388242+Copiums@users.noreply.github.com>
2026-05-11 15:58:31 -0400

    Update Network.lua

--- Modules/Network.lua
+++ Modules/Network.lua
@@ -1,45 +1,57 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local lplr = playersService.LocalPlayer 
-local Network = {
-    Info = {}
+local Network: table? = {
+        Info = {}
 }
+local cloneref: (obj: any) -> any = cloneref or function(obj)
+    	return obj;
+end;
+local replicatedStorage: ReplicatedStorage = cloneref(game:GetService("ReplicatedStorage"));
+local Event: any = replicatedStorage:WaitForChild("Event")
 
-function Network:Receive(args)
-    local name = args[1]
-    if name == "shoot" or name == "bomb" then
-        -- print("shooting is now"..args[2][1])
-        self.Info["shooting"] = args[2]
-    end
-end
-
-local Event = ReplicatedStorage:WaitForChild("Event")
-function Network:Send(name, ...)
-    Event:FireServer(name, {...}, true)
-end
-
-function Network:Destroy()
-    self.gmt.__namecall = self.namecall
-    return true
-end
-
-function Network:Init()
-    local gmt = getrawmetatable(game)
-    setreadonly(gmt, false)
-    self.gmt = gmt
-    local namecall = gmt.__namecall
-    self.namecall = namecall
-    gmt.__namecall = function(event, ...)
-        if event ~= Event then
-            return namecall(event, ...)
+local Receive: (args: any) -> () = function(args: any)
+        local name: string = args[1]
+        if name == "shoot" or name == "bomb" then
+                -- print("shooting is now"..args[2][1])
+                Network.Info["shooting"] = args[2]
         end
-        local method = getnamecallmethod()
-        if tostring(method) ~= "FireServer" then
-            return namecall(event, ...)
-        end
-        local args = {...}
-        self:Receive(args)
-        return namecall(event, ...)
-    end
 end
+
+local Send: (name: string, ...: any) -> () = function(name: string, ...)
+        Event:FireServer(name, {...}, true)
+end
+
+local Destroy: () -> boolean = function()
+        if Network.gmt and Network.namecall then
+                Network.gmt.__namecall = Network.namecall
+        end
+        return true
+end
+
+local Init: () -> () = function()
+        local gmt: any = getrawmetatable(game)
+        setreadonly(gmt, false)
+        
+        Network.gmt = gmt
+        Network.namecall = gmt.__namecall
+        
+        gmt.__namecall = function(event, ...)
+                if event ~= Event then
+                        return Network.namecall(event, ...)
+                end
+                
+                local method: string = getnamecallmethod()
+                if tostring(method) ~= "FireServer" then
+                        return Network.namecall(event, ...)
+                end
+                
+                local args: any = {...}
+                Receive(args)
+                return Network.namecall(event, ...)
+        end
+end
+
+Network.Receive = Receive
+Network.Send = Send
+Network.Destroy = Destroy
+Network.Init = Init
 
 return Network
