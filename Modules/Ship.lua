147b359d1bfd703e0e995ba0b167af31e80149bb
Copium <119388242+Copiums@users.noreply.github.com>
2026-05-12 22:35:37 -0400

    Update Ship.lua

--- Modules/Ship.lua
+++ Modules/Ship.lua
@@ -1,77 +1,75 @@
-local Ship = {
+local Ship: table = {
         Status = {CurrentGun = -5},
         Ship = nil,
         ShipNames = {
                 "Heavy Cruiser", "Cruiser", "Carrier", "Destroyer", "Battleship", "Submarine"
         }
-}
+};
 
-local shipNames: table = Ship.ShipNames
-
-local debugLog: table = {}
+local shipNames: table = Ship.ShipNames;
+local debugLog: table = {};
 
 local function logDebug(msg: string)
-        local entry = os.clock() .. " | " .. msg
-        table.insert(debugLog, entry)
-        print(entry)
+        local entry = os.clock() .. " | " .. msg;
+        table.insert(debugLog, entry);
+        print(entry);
         if #debugLog > 200 then
-                writefile("ship_debug.txt", table.concat(debugLog, "\n"))
-        end
-end
+                writefile("ship_debug.txt", table.concat(debugLog, "\n"));
+        end;
+end;
 
 function Ship:UpdateShip(seatPart : BasePart)
         if not seatPart then
-                logDebug("UpdateShip: seatPart is nil, clearing ship")
-                self.Ship = nil
-                return
-        end
+                logDebug("UpdateShip: seatPart is nil, clearing ship");
+                self.Ship = nil;
+                return;
+        end;
         local model = seatPart.Parent
         if not model then
-                logDebug("UpdateShip: model is nil")
-                return
-        end
-        logDebug("UpdateShip: model.Name = " .. model.Name)
+                logDebug("UpdateShip: model is nil");
+                return;
+        end;
+        logDebug("UpdateShip: model.Name = " .. model.Name);
         if table.find(shipNames, model.Name) then
-                self.Ship = model
-                logDebug("UpdateShip: ship set to " .. model.Name)
+                self.Ship = model;
+                logDebug("UpdateShip: ship set to " .. model.Name);
         else
-                logDebug("UpdateShip: model not in shipNames, ignoring")
-        end
-end
+                logDebug("UpdateShip: model not in shipNames, ignoring");
+        end;
+end;
 
 function Ship:GetGunBarrel(gunIndex : number)
         if not Ship.Ship then
-                logDebug("GetGunBarrel: Ship.Ship is nil")
-                return nil
-        end
+                logDebug("GetGunBarrel: Ship.Ship is nil");
+                return nil;
+        end;
 
         for _, turret in next, self.Ship:GetChildren() do
                 if turret.Name == "Turret" and turret:IsA("Model") then
-                        local seat = turret:FindFirstChild("SBTurretSeat")
-                        local gunType = seat and seat:FindFirstChild("GunType")
-                        local isAA = gunType and gunType.Value == "AA"
-
+                        local seat = turret:FindFirstChild("SBTurretSeat");
+                        local gunType = seat and seat:FindFirstChild("GunType");
+                        local isAA = gunType and gunType.Value == "AA";
                         if gunIndex == 0 and not isAA then
-                                local bright = turret:FindFirstChild("BRight")
-                                logDebug("GetGunBarrel[0]: found main gun turret, BRight = " .. tostring(bright))
-                                return bright
+                                local bright = turret:FindFirstChild("BRight");
+                                logDebug("GetGunBarrel[0]: found main gun turret, BRight = " .. tostring(bright));
+                                return bright;
                         elseif gunIndex == 1 and isAA then
-                                local bright = turret:FindFirstChild("BRight")
-                                logDebug("GetGunBarrel[1]: found AA turret, BRight = " .. tostring(bright))
-                                return bright
-                        end
-                end
-        end
+                                local bright = turret:FindFirstChild("BRight");
+                                logDebug("GetGunBarrel[1]: found AA turret, BRight = " .. tostring(bright));
+                                return bright;
+                        end;
+                end;
+        end;
 
-        logDebug("GetGunBarrel[" .. gunIndex .. "]: nothing found")
-        return nil
-end
+        logDebug("GetGunBarrel[" .. gunIndex .. "]: nothing found");
+        return nil;
+end;
 
 function Ship:CurrentGun()
         if not Ship.Ship then
-                return nil
-        end
-        return Ship.Ship.GunNum.Value
-end
+                return nil;
+        end;
+        return Ship.Ship.GunNum.Value;
+end;
 
-return Ship
+return Ship;
